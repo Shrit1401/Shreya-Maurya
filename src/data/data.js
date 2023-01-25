eb863c1b17250c64aa7598191f13ae79be7219a7
Anvay Arora <75296875+the-protagonist-view@users.noreply.github.com>
2023-01-25 10:59:17 +0530

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -30,7 +30,7 @@ export const work = [
     desc: "B2I / live events / team building / employment branding",
   },
   {
-    title: "wisa",
+    title: "This Too Shall Pass",
     imageUrl:
       "https://jolos.eu/wp-content/uploads/2022/11/11Jun2022_213252_MMP7300_shotbyMarekMetslaid-1280x852.jpg.webp",
     desc: "B2I / live events / team building / employment branding",
